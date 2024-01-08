0edf8322600aa469a8d8bb561af2b7c48c3889ab
Devent <tavata.canales@gmail.com>
2024-01-08 11:32:24 -0500

    Se el metodo toggleFavorites

--- src/store/pokemons/pokemons.ts
+++ src/store/pokemons/pokemons.ts
@@ -1,22 +1,34 @@
 import { simplePokemon } from '@/app/pokemons';
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 
-interface PokemonState{
-    [key:string]:simplePokemon
-} 
+interface PokemonState {
+    [key: string]: simplePokemon
+}
 
-const initialState:PokemonState = {
-    '1':{id:'1',name:'bulbasaur'},
-    '2':{id:'2',name:'bulbasaur'}
+const initialState: PokemonState = {
+    '1': { id: '1', name: 'bulbasaur' },
+    '2': { id: '2', name: 'bulbasaur' }
 }
 
 const pokemonsSlice = createSlice({
-  name: 'pokemons',
-  initialState,
-  reducers: {}
+    name: 'pokemons',
+    initialState,
+    reducers: {
+
+        toggleFavorites(state, action: PayloadAction<simplePokemon>) {
+            const pokemon = action.payload;
+            const { id } = pokemon;
+            if (!!state[id]) {//TODO: ESTA EXPRESION FIJA SI EXISTE 
+                delete state[id]
+                return;
+            }
+            state[id] = pokemon //FIXME: ESTA EXPRESION NO LA DOMINO BIEN EXPANDIRSE
+
+        }
+    }
 });
 
-export const {} = pokemonsSlice.actions
+export const { toggleFavorites } = pokemonsSlice.actions
 
 export default pokemonsSlice.reducer
