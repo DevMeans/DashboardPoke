e184873a3f20b3b0f71ecd9cd53eba7482e20ead
Devent <tavata.canales@gmail.com>
2024-01-08 10:39:52 -0500

    Configuracion pokemon reducer añadido

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './counter/counterSlice'
+import pokemonReducer from './pokemons/pokemons'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 export const store = configureStore({
     reducer: {
-        counter: counterReducer
+        counter: counterReducer,
+        pokemons: pokemonReducer
     },
 })
 
